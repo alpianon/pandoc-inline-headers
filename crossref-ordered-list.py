95ba2964896f5278a89e2dd41042c3275c2d95a0
Alberto Pianon <alberto@pianon.eu>
2019-06-24 11:23:26 +0200

    added support for custom identifiers in ordered lists

--- crossref-ordered-list.py
+++ crossref-ordered-list.py
@@ -2,7 +2,7 @@
 
 import panflute as pf
 
-import sys
+import re
 
 def upperRoman(number):
     roman_digit_map = [
@@ -48,6 +48,7 @@ def action(elem, doc):
     numberSections = doc.get_metadata('numberSections')
     sectionsDepth = doc.get_metadata('sectionsDepth')
     sectionsDepth = int(sectionsDepth) if sectionsDepth else 0
+    secHeaderDelim = doc.get_metadata('secHeaderDelim')
     if not numberSections:
         return None
     if isinstance(elem, pf.Header):
@@ -60,28 +61,63 @@ def action(elem, doc):
         format_number = number_formats[ordered_list.style]
         for idx, list_item in enumerate(ordered_list.content):
             number = format_number(idx+1)
-            elem.parent.content.insert(
-                elem.index+1,
-                pf.Header(
+            if list_item.content:
+                list_item_text = pf.stringify(list_item)
+                r = re.search("\{.*\}", list_item_text)
+                metadata = r[0] if r else None
+            else:
+                list_item_text = ""
+                metadata = None
+            if metadata:
+                identifier_set_by_user = [
+                    i
+                    for i in metadata[1:-1].split(" ")
+                    if i.startswith("#")
+                ]
+                s = list_item_text.split(metadata)
+                header_text = s[0]
+                para_text = s[1]
+                header_str = "{} {} {} {}".format(
+                    "#" * (curlevel+1),
+                    (
+                        number+(secHeaderDelim if header_text else "")
+                        if (curlevel >= sectionsDepth and sectionsDepth !=-1)
+                        else ""
+                    ),
+                    header_text,
+                    metadata
+                )
+                header = pf.convert_text(header_str)[0]
+                header.attributes['label'] = number
+                if not identifier_set_by_user:
+                    header.identifier = curid+":"+number
+                c = pf.convert_text(s[1])
+                para = c[0] if c else None
+                # convert text returns a list with 1 element
+                # (or no element of converted text is empty)
+            else:
+                header = pf.Header(
                     (
                         pf.Str(number)
-                        if (curlevel >= sectionsDepth and sectionsDepth != -1)
+                        if (curlevel >= sectionsDepth and sectionsDepth !=-1)
                         else pf.Str("")
                     ),
                     level=curlevel+1,
                     identifier=curid+":"+number,
                     attributes={'label': number},
-                ),
-            )
+                )
+                c = pf.convert_text(list_item_text)
+                para = c[0] if c else None
+                # convert text returns a list with 1 element
+                # (or no element of converted text is empty)
+
+            elem.parent.content.insert(elem.index+1, header)
             elem = elem.next
-            elem.parent.content.insert(
-                elem.index+1,
-                pf.Para()
-            )
-            elem = elem.next
-            if list_item.content:
-                elem.content = list_item.content[0].content
-        while len(ordered_list.content) > 0:
+            if para:
+                elem.parent.content.insert(elem.index+1, para)
+                elem = elem.next
+
+        while len(ordered_list.content) > 0: # delete ordered list
             ordered_list.content.pop(0)
 
 if __name__ == '__main__':
